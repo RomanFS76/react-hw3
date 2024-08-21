52545af1dd5b190c6b3a9e04d34e3685f5a93d32
RomanFS76 <korvy85@gmail.com>
2024-08-21 22:02:33 +0300

    init

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Contact from "../Contact/Contact";
+
+const ContactList = () => {
+  return (
+    <>
+      <Contact />
+    </>
+  );
+};
+
+export default ContactList;
